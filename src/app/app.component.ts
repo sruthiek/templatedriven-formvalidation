3d91ee609c05d741682b371073f7ff3b84256e67
sruthiek <sruthi.ek2580@gmail.com>
2023-11-28 16:25:08 +0530

    template driven form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'templatedriven-formvalidation';
+
+
+
+mobNumberPattern = "^((\\+91-?)|0)?[0-9]{10}$";  
+passwordPtn ='^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,16}$'
+emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+
+
+
+userData: any = {}  
+
+onSubmit(form: any) {
+  console.log('form submitted',form)
+  alert('Form saved sucessfully!');
+  // x.reset();
+}
+
+  
 }
